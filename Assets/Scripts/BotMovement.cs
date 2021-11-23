6a2188a3e8eabfde7677067c225b77438d234e60
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2021-11-23 19:18:06 +0200

    bot prefab

--- Assets/Scripts/BotMovement.cs
+++ Assets/Scripts/BotMovement.cs
@@ -9,5 +9,14 @@ public class BotMovement : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+
+        if (transform.position.x > 38.62f)
+        {
+            transform.position = new Vector3(38.62f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < 32.07f)
+        {
+            transform.position = new Vector3(32.07f, transform.position.y, transform.position.z);
+        }
     }
 }
