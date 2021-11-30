173f11141e8896df3f04ae138acbb8c5c28d3be5
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2021-11-30 16:50:43 +0200

    attempt to lerp

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -11,11 +11,12 @@ public class DataHandler : MonoBehaviour
             if (DataCollector.countRight >= DataCollector.countLeft)
             {
                 transform.parent.Translate(Vector3.right * 1f);
-                
+                //transform.parent.position += Vector3.Lerp(Vector3.right, Vector3.right, 0.5f);
             }
             else
             {
                 transform.parent.Translate(Vector3.left * 1f);
+                //transform.parent.position += Vector3.Lerp(Vector3.left, Vector3.left, 0.5f);
             }
         }
     }
