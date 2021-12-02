2c9e6222500321be1cd083554c8d7d5ae8eb5e5f
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2021-12-02 22:27:16 +0200

    smooth bot movement

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class DataHandler : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public float sideSpeed = 0.03f;
+
+    private void OnTriggerStay(Collider other)
     {
         if (!other.gameObject.CompareTag("Untagged"))
         {
             if (DataCollector.countRight >= DataCollector.countLeft)
             {
-                transform.parent.Translate(Vector3.right * 1f);
-                //transform.parent.position += Vector3.Lerp(Vector3.right, Vector3.right, 0.5f);
+                transform.parent.position += Vector3.right * sideSpeed;
             }
             else
             {
-                transform.parent.Translate(Vector3.left * 1f);
-                //transform.parent.position += Vector3.Lerp(Vector3.left, Vector3.left, 0.5f);
+                transform.parent.position += Vector3.left * sideSpeed;
             }
         }
     }
